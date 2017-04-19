ae9f9d472c7222cb629418cebf96def3ce13ec16
aty1990 <893466820@qq.com>
2017-04-19 13:00:00 +0800

    路由配置

--- src/main.js
+++ src/main.js
@@ -4,15 +4,27 @@ import mui from '../js/mui.min.js'
 import App from './App'
 import '@/assets/css/common'
 
+
 Vue.config.productionTip = false
 window.mui=mui
 
 mui.init()
 
+mui.plusReady(function(){ 
+        
+    if(plus.networkinfo.getCurrentType()!=3){
+    	// 3为状态码，wifi状态码是3
+        mui.toast("当前为3G/4G网络")
+    }else{
+    	mui.toast("当前为wifi网络")
+    }
+    if (plus.networkinfo.getCurrentType() == plus.networkinfo.CONNECTION_NONE) {
+		mui.toast("网络异常，请检查网络设置！")
+	}
+})
 if(process.env.NODE_ENV==="development"){
 	require('@/assets/css/mui.css') 
 }
-
 
 /* eslint-disable no-new */
 new Vue({ 
