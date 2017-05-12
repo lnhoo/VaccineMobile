bb07bdb77df2d301ca4930f67b181f45e611542b
aty1990 <893466820@qq.com>
2017-05-12 18:17:43 +0800

    定位错误信息提示

--- src/pages/layout/mine.vue
+++ src/pages/layout/mine.vue
@@ -12,21 +12,45 @@
 	      	return {}
 	    },
 	    mounted() {
-	    	let map = new BMap.Map("allMap");
-	    	var point = new BMap.Point(116.331398,39.897445);
-				map.centerAndZoom(point,12);
-			plus.geolocation.getCurrentPosition( function ( p ) {
-				var lo = new BMap.Point(p.coords.longitude,p.coords.latitude);
-				var mk = new BMap.Marker( lo );
-					map.addOverlay( mk );
-					map.panTo( lo );
-					var infoWindow = new BMap.InfoWindow("我的位置", { 
-						offset : new BMap.Size(0,-25)
-					});    // 创建信息窗口对象
-					map.openInfoWindow(infoWindow,lo); 
-			}, function ( e ) {
-				mui.toast( "Geolocation error: " + e.message );
-			} );	
+			this.initData()
+	    },
+	    methods:{
+	    	initData(){
+	    		var options = {
+				  	enableHighAccuracy: true,
+				  	timeout: 5000,
+				  	maximumAge: 0
+				};
+				function success(pos) {
+				  	var crd = pos.coords;
+				  	var a = new BMap.Point(crd.longitude,crd.latitude);
+				  	let map = new BMap.Map("allMap");
+		    		var point = new BMap.Point(116.331398,39.897445);
+					map.centerAndZoom(point,15);
+					BMap.Convertor.translate( a , 0, function(data){
+						var lo = new BMap.Point(data.lng,data.lat);
+						var mk = new BMap.Marker( lo );
+							var label = new BMap.Label("我的位置",{offset:new BMap.Size(20,-10)});
+    						mk.setLabel(label);
+    						map.addOverlay( mk );
+    						map.panTo( lo );
+					})	
+				};
+				function error(err) {
+				  	switch(error.code){
+			            case error.TIMEOUT:
+			                mui.toast("连接超时，请重试");
+			                break;
+			            case error.PERMISSION_DENIED:
+			                mui.toast("您拒绝了使用位置共享服务，查询已取消");
+			                break;
+			            case error.POSITION_UNAVAILABLE:
+			                mui.toast("定位失败，请查看应用是否授权");
+			                break;
+			        }
+				};
+				navigator.geolocation.getCurrentPosition(success, error, options);
+	    	}
 	    }
 	}
 </script>
