7ddf4c90b0e49d1267bc3bdf725bd6a5efada163
aty1990 <893466820@qq.com>
2017-05-28 14:39:42 +0800

    sass配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -20,7 +20,7 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json','.css','.less'],
+    extensions: ['.js', '.vue', '.json','.css','.scss'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
